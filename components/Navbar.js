82c7824fa1bd18b6bc62641fdf8334cfb4d4586a
wardenein <96897527+wardenein@users.noreply.github.com>
2025-09-26 15:07:48 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import Link from "next/link"
 
 export default function Navbar() {
@@ -8,6 +7,10 @@ export default function Navbar() {
         <li><Link href="/">Home</Link></li>
         <li><Link href="/workout">Workouts</Link></li>
         <li><Link href="/food">Food</Link></li>
+        <li><Link href="/supps">Supplements</Link></li>
+        <li><Link href="/plan">Plans</Link></li>
+        <li><Link href="/rank">Ranks</Link></li>
+        <li><Link href="/info">Info</Link></li>
       </ul>
     </nav>
   )
