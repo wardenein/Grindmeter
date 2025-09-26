bd734891defef6abf2b7be55cdc7ccfefb70dea7
wardenein <96897527+wardenein@users.noreply.github.com>
2025-09-26 15:19:55 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,9 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav style={{ padding: 12, borderBottom: "1px solid #ddd" }}>
+      <ul style={{ display: "flex", gap: 20, listStyle: "none", margin: 0, padding: 0 }}>
         <li><Link href="/">Home</Link></li>
         <li><Link href="/workout">Workouts</Link></li>
         <li><Link href="/food">Food</Link></li>
@@ -13,5 +13,5 @@ export default function Navbar() {
         <li><Link href="/info">Info</Link></li>
       </ul>
     </nav>
-  )
+  );
 }
