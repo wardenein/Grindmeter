ab9e0b29c73ce9695dcfe74e58589cf1c3ef7a5d
wardenein <96897527+wardenein@users.noreply.github.com>
2025-10-01 12:25:43 -0400

    Update index.js

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,36 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Grindmeter</h1>
-      <p>This will be your dashboard.</p>
+      <h1>Welcome to Grindmeter!</h1>
+      <p>This is your frontend running locally.</p>
     </div>
   )
 }
-
-
-<!DOCTYPE html>
-<html lang="eng">
-<head>
-    <meta charset="UTF-8">
-    <title>Grindmeter</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-  <nav class="navbar">
-    <ul class="navbar-list">
-        <li><a href="/Grindmeter/index.html">Home</a></li>
-        <li><a href="/Grindmeter/workout.html">Workout Tracker</a></li>
-        <li><a href="/Grindmeter/plan.html">Plan Generator</a></li>
-        <li><a href="/Grindmeter/food.html">Food & Supp. tracker</a></li>
-        <li><a href="/Grindmeter/info.html">Informations</a></li>
-    </ul>
-</nav>
-    
-    <h1>Welcome to Grindmeter</h1>
-    <p>Where everything is in one place</p>
-    <button onclick="sayHello()">Click me</button>
-    <script src="script.js"></script>
-    
-</body>
-</html>
